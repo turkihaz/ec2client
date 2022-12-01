2f56a149c92f3faa4970c6a2a09c17a6af14fb22
turkihaz <turkihaz@users.noreply.github.com>
2022-12-01 06:53:05 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ subscriberAt1.on('message', function (topic, message) {
 
  
 var subscriberAt2  = mqtt.connect("mqtt://18.182.60.4:1884")
-subscriberAt2.subscribe("temp")
+subscriberAt2.subscribe("temp2")
 subscriberAt2.on('message', function (topic, message) {
     // message is Buffer
     var json = JSON.parse(message.toString());
@@ -35,15 +35,21 @@ console.log("Sub2: " + ms)
 
 
 var publisherAt1  = mqtt.connect("mqtt://35.77.103.62:1883")
+var publisherAt2  = mqtt.connect("mqtt://35.77.103.62:1883")
 
 var messageAt1 = {
   sensor: "sensor 1",
   temp: 24.8,
   timestamp: Date.now()
 }
-
+var messageAt1 = {
+  sensor: "sensor 2",
+  temp: 24.8,
+  timestamp: Date.now()
+}
 for (let index = 0; index < 10; index++) {
   publisherAt1.publish("temp",JSON.stringify(messageAt1));
+  publisherAt2.publish("temp2",JSON.stringify(messageAt2));
   
   
 }
